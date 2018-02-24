7043a4174afa3bda65366e425221e60f22a60d74
Yahiaoui Rachid <rac021@hotmail.com>
2018-02-25 00:48:13 +0100

    Update 03_extract_prefixs_from_owl.sh

--- src/core/pipeline/scripts/03_extract_prefixs_from_owl.sh
+++ src/core/pipeline/scripts/03_extract_prefixs_from_owl.sh
@@ -39,7 +39,7 @@
          help)  echo
 		echo " Total Arguments : Two                    "
                 echo
-		echo "   ontology=    :  IP Path                "
+		echo "   ontology=    :  Ontology file path     "
 		echo "   prefixFile=  :  output File of prefixs "
 		echo
                 EXIT;
