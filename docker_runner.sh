1eb2fc6284b947dd67c77aab65bf28a79a7c506f
Yahiaoui Rachid <rac021@hotmail.com>
2018-02-27 09:56:10 +0100

    Create docker_runner.sh

---
+++ docker_runner.sh
@@ -0,0 +1,6 @@
+
+
+docker run -it --net host                                                \
+           -v `pwd`/src/orchestrators/.:/opt/coby/pipeline/orchestrators \
+           -v `pwd`/src/SI:/opt/coby/pipeline/SI coby                    \
+           /opt/coby/pipeline/orchestrators/synthesis_extractor_entry.sh
