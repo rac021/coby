5f5f648498715e72e65edbdfc0c52d1df1742910
Yahiaoui Rachid <rac021@hotmail.com>
2018-02-27 10:11:10 +0100

    Update docker_runner.sh

--- docker_runner.sh
+++ docker_runner.sh
@@ -4,3 +4,5 @@ docker run -it --net host                                                \
            -v `pwd`/src/orchestrators/.:/opt/coby/pipeline/orchestrators \
            -v `pwd`/src/SI:/opt/coby/pipeline/SI coby                    \
            /opt/coby/pipeline/orchestrators/synthesis_extractor_entry.sh
+
+
