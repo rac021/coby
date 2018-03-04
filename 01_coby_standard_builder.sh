53b2ef0e95f7e25b8caafdfb0b233304957db3c2
Yahiaoui Rachid <rac021@hotmail.com>
2018-03-04 04:46:01 +0100

    Update 01_coby_standard_builder.sh

--- 01_coby_standard_builder.sh
+++ 01_coby_standard_builder.sh
@@ -7,6 +7,17 @@
     echo "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
  }
 
+ if [ "$1" = "help" -o "$1" = "h" -o "$1" == "H" -o "$1" = "HELP" ] ; then 
+     echo
+     echo " Install Cmd Exp  : "
+     echo "  1- Generate Only Coby Package ( without downloading and compiling projects ):  $0  "
+     echo "  2- install only coby       :  $0 -i coby      "
+     echo "  3- install only jaxy       :  $0 -i jaxy      "
+     echo "  4- install coby + jaxy     :  $0 -i coby jaxy "
+     echo 
+     exit
+fi
+
  # Prepare Installation folder 
 
  COBY_SOURCES="src"
@@ -20,34 +31,213 @@
  SCRIPTS_PATH_DESTINATION="$COBY_BINARY/scripts"
 
  rm    -rf $COBY_BINARY_ROOT
- mkdir -p  $COBY_BINARY  
- mkdir     $COBY_LIBS
- mkdir     $COBY_LIBS/logs
- mkdir     $COBY_BINARY/work-tmp
 
- mkdir -p $COBY_BINARY_ROOT/jax-Y-ws/Jax-Y-Coby
- mkdir -p $COBY_BINARY_ROOT/jax-Y-ws/Jax-Y-Client
+ if [ $# == 0 ] ; then 
 
- # Copy all scripts to the Binary Directory 
- tput setaf 2
+   mkdir -p  $COBY_BINARY  
+   mkdir     $COBY_LIBS
+   mkdir     $COBY_LIBS/logs
+   mkdir     $COBY_BINARY/work-tmp
+
+   # Copy all scripts to the Binary Directory 
+   tput setaf 2
+   echo 
+   echo " ###########################                             "
+   echo " ##### Copy scripts core ###                             "
+   echo 
+   echo -e "\e[90m Location   : $SCRIPTS_PATH_SOURCE       \e[32m "
+   echo -e "\e[90m Destination : $SCRIPTS_PATH_DESTINATION \e[32m "
+   echo
+   echo " ###########################                             "
+   echo 
+   sleep 0.5
+   tput setaf 7
+
+   cp -rf $SCRIPTS_PATH_SOURCE $SCRIPTS_PATH_DESTINATION
+
+   echo -e "\e[90m Copied. Done. \e[32m "
+   echo
+   echo " Next Step :  Install libs "
+   echo " Command : ./$SCRIPTS_PATH_DESTINATION/00_install_libs.sh help "
+   
+ fi
+
+
+ if [ "$1" = "-i" ] && [ "$2" = "coby" -o "$3" = "coby" ] ; then 
+
+   mkdir -p  $COBY_BINARY  
+   mkdir     $COBY_LIBS
+   mkdir     $COBY_LIBS/logs
+   mkdir     $COBY_BINARY/work-tmp
+
+   # Copy all scripts to the Binary Directory 
+   tput setaf 2
+   echo 
+   echo " ###########################                             "
+   echo " ##### Copy scripts core ###                             "
+   echo 
+   echo -e "\e[90m Location   : $SCRIPTS_PATH_SOURCE       \e[32m "
+   echo -e "\e[90m Destination : $SCRIPTS_PATH_DESTINATION \e[32m "
+   echo
+   echo " ###########################                             "
+   echo 
+   sleep 0.5
+   tput setaf 7
+
+   cp -rf $SCRIPTS_PATH_SOURCE $SCRIPTS_PATH_DESTINATION
+
+   echo -e "\e[90m Copied. Done. \e[32m "
+   echo
+
+   echo -e "\e[90m Run Instalation ..\e[32m "
+
+   ./$SCRIPTS_PATH_DESTINATION/00_install_libs.sh
+
+ fi
+
+ if [ "$1" = "-i" ] && [ "$2" = "jaxy" -o "$3" = "jaxy" ] ; then   
+
+   CURRENT_PATH=`pwd`
+ 
+   # Prepare Installation folder 
+
+   JAXY_JAVA_PROJECT_PATH="$COBY_CORE/jax-Y-ws"
+
+   # jax-Y Web service
+
+   # Dependencies 
+   JAVA_PROJECT_JAXY_API="$JAXY_JAVA_PROJECT_PATH/dep/01_G-Jax-Swar-Api"
+   JAVA_PROJECT_JAXY_SECURITY="$JAXY_JAVA_PROJECT_PATH/dep/02_G-Jax-Security-Provider"
+   JAVA_PROJECT_JAXY_DISCOVERY="$JAXY_JAVA_PROJECT_PATH/dep/03_G-Jax-Service-Discovery"
+   # Impl
+   JAVA_PROJECT_JAXY_COBY="$JAXY_JAVA_PROJECT_PATH/Jax-Y-Coby"
+   JAVA_PROJECT_JAXY_CLIENT="$JAXY_JAVA_PROJECT_PATH/Jax-Y-Client"
+ 
+   JAXY_SERVER_PATH="$COBY_BINARY_ROOT/jax-y_server"
+   JAXY_SERVER_NAME="jax-y-swarm.jar"
+   JAXY_CLIENT_PATH="$COBY_BINARY_ROOT/jax-y_client"
+   JAXY_CLIENT_NAME="g-jax-client.jar"
+
+   mkdir     $JAXY_SERVER_PATH
+   mkdir     $JAXY_CLIENT_PATH
+
+   TMP_COMPILATION_FOLDER="$COBY_BINARY_ROOT/tmp"
+ 
+   mkdir     $TMP_COMPILATION_FOLDER/
+  
+ 
+   ############################
+   ### Compile Web Serivces ###
+   ############################
+
+   # 4- Compile jax-Y 
+
+   tput setaf 2
+   echo 
+   echo " ###########################                         "
+   echo " ##### Install jax-Y-API ###                         "
+   echo 
+   echo -e "\e[90m Location :  $TMP_COMPILATION_FOLDER \e[32m "
+   echo
+   echo " ###########################                         "
+   echo 
+   sleep 2
+   tput setaf 7
+
+   cp -a $JAVA_PROJECT_JAXY_API/. $TMP_COMPILATION_FOLDER/ 
+   cd  $TMP_COMPILATION_FOLDER/ && mvn clean install &&  cd  $CURRENT_PATH
+   rm -rf $TMP_COMPILATION_FOLDER/{,.[!.],..?}*
+
+
+   tput setaf 2
+   echo 
+   echo " ###########################                         "
+   echo " ##### Install jax-Y-Sec ###                         "
+   echo 
+   echo -e "\e[90m Location :  $TMP_COMPILATION_FOLDER \e[32m "
+   echo
+   echo " ###########################                         "
+   echo 
+   sleep 2
+   tput setaf 7
+
+   cp -a $JAVA_PROJECT_JAXY_SECURITY/. $TMP_COMPILATION_FOLDER/ 
+   cd  $TMP_COMPILATION_FOLDER/ && mvn clean install &&  cd  $CURRENT_PATH
+   rm -rf $TMP_COMPILATION_FOLDER/{,.[!.],..?}*
+
+   tput setaf 2
+   echo 
+   echo " ############################                         "
+   echo " ##### Install jax-Y-Disc ###                         "
+   echo 
+   echo -e "\e[90m Location :  $TMP_COMPILATION_FOLDER \e[32m  "
+   echo
+   echo " ############################                         "
+   echo 
+   sleep 2
+   tput setaf 7
+
+   cp -a $JAVA_PROJECT_JAXY_DISCOVERY/. $TMP_COMPILATION_FOLDER/ 
+   cd  $TMP_COMPILATION_FOLDER/ && mvn clean install && cd $CURRENT_PATH
+   rm -rf $TMP_COMPILATION_FOLDER/{,.[!.],..?}*
+
+   tput setaf 2
+   echo 
+   echo " ############################                         "
+   echo " ##### Install jax-Y-COBY ###                         "
+   echo 
+   echo -e "\e[90m Location :  $TMP_COMPILATION_FOLDER \e[32m  "
+   echo
+   echo " ############################                         "
+   echo 
+   sleep 2
+   tput setaf 7
+
+   cp -a $JAVA_PROJECT_JAXY_COBY/. $TMP_COMPILATION_FOLDER/ 
+   cd  $TMP_COMPILATION_FOLDER/ &&  mvn clean package && cd  $CURRENT_PATH
+
+   cp $TMP_COMPILATION_FOLDER/target/$JAXY_SERVER_NAME $JAXY_SERVER_PATH 
+
+   cp -r $JAVA_PROJECT_JAXY_COBY/db-script           $JAXY_SERVER_PATH
+   cp $JAVA_PROJECT_JAXY_COBY/coby_config.properties $JAXY_SERVER_PATH
+   cp $JAVA_PROJECT_JAXY_COBY/serviceConf.yaml       $JAXY_SERVER_PATH
+   cp $JAVA_PROJECT_JAXY_COBY/run_server.sh          $JAXY_SERVER_PATH
+ 
+   rm -rf $TMP_COMPILATION_FOLDER/{,.[!.],..?}*
+ 
+   tput setaf 2
+   echo 
+   echo " ############################                         "
+   echo " ##### Install jax-Y-CLI ###                          "
+   echo 
+   echo -e "\e[90m Location :  $TMP_COMPILATION_FOLDER \e[32m  "
+   echo
+   echo " ############################                         "
+   echo 
+   sleep 2
+   tput setaf 7
+
+   cp -a $JAVA_PROJECT_JAXY_CLIENT/. $TMP_COMPILATION_FOLDER/ 
+   cd  $TMP_COMPILATION_FOLDER/  && mvn clean compile assembly:single && cd $CURRENT_PATH
+   
+   cp $TMP_COMPILATION_FOLDER/target/$JAXY_CLIENT_NAME $JAXY_CLIENT_PATH 
+   rm -rf $TMP_COMPILATION_FOLDER/{,.[!.],..?}*
+
+   rm -rf $TMP_COMPILATION_FOLDER
+
+ fi
+ 
+ if [ "$2" = "jaxy" -o "$3" = "jaxy" ] ; then  ## Redisplay again the message ABOUT the ORCHESTRATORS
+ 
+   tput setaf 2
+   echo 
+   echo 
+   echo " ###  Before you start using COBY, be sure to provide your 'ORCHESTRATORS' and 'SI' folders ### "
+   sleep 1
+   tput setaf 7
+   
+ fi
+ 
  echo 
- echo " ###########################                             "
- echo " ##### Copy scripts core ###                             "
- echo 
- echo -e "\e[90m Location   : $SCRIPTS_PATH_SOURCE       \e[32m "
- echo -e "\e[90m Destination : $SCRIPTS_PATH_DESTINATION \e[32m "
- echo
- echo " ###########################                             "
- echo 
- sleep 0.5
- tput setaf 7
-
- cp -rf $SCRIPTS_PATH_SOURCE $SCRIPTS_PATH_DESTINATION
-
- echo -e "\e[90m Copied. Done. \e[32m "
- echo
  echo " Done !"
- echo
- echo " Next Step :  Install libs "
- echo " Command : ./$SCRIPTS_PATH_DESTINATION/00_install_libs.sh help "
  echo 
