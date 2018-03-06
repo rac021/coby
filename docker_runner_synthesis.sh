779fee69b98aa693c296d4fa47bbace43980761c
Yahiaoui Rachid <rac021@hotmail.com>
2018-03-06 15:32:10 +0100

    Update docker_runner_synthesis.sh

--- docker_runner_synthesis.sh
+++ docker_runner_synthesis.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 docker run -it --net host                                                \
-           --memory-swap=0                                               \
            --memory-swappiness=0                                         \
            -v `pwd`/src/orchestrators/.:/opt/coby/pipeline/orchestrators \
            -v `pwd`/src/SI:/opt/coby/pipeline/SI coby                    \
