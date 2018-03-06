e3ec74aebcac9e44901d4db79371d96231f500c8
Yahiaoui Rachid <rac021@hotmail.com>
2018-03-06 15:49:32 +0100

    Update docker_runner_synthesis.sh

--- docker_runner_synthesis.sh
+++ docker_runner_synthesis.sh
@@ -2,6 +2,7 @@
 
 docker run -it --net host                                                \
            --memory-swappiness=0                                         \
+            --ulimit nproc=20000:50000                                   \
            -v `pwd`/src/orchestrators/.:/opt/coby/pipeline/orchestrators \
            -v `pwd`/src/SI:/opt/coby/pipeline/SI coby                    \
            /opt/coby/pipeline/orchestrators/synthesis_extractor_entry.sh
