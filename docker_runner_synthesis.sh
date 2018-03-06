7fdd6f71b3f7b7726bbfcb2fef731d1f4db04aff
Yahiaoui Rachid <rac021@hotmail.com>
2018-03-06 15:27:06 +0100

    Update and rename docker_runner.sh to docker_runner_synthesis.sh

--- docker_runner_synthesis.sh
+++ docker_runner_synthesis.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 docker run -it --net host                                                \
+           --memory-swap=0                                               \
+           --memory-swappiness=0                                         \
            -v `pwd`/src/orchestrators/.:/opt/coby/pipeline/orchestrators \
            -v `pwd`/src/SI:/opt/coby/pipeline/SI coby                    \
            /opt/coby/pipeline/orchestrators/synthesis_extractor_entry.sh
