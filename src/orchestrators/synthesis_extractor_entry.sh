aa0366c63719b13dfd8f1501dc6f37d6674f867d
Yahiaoui Rachid <rac021@hotmail.com>
2018-03-10 00:39:23 +0100

    Update synthesis_extractor_entry.sh

--- src/orchestrators/synthesis_extractor_entry.sh
+++ src/orchestrators/synthesis_extractor_entry.sh
@@ -3,6 +3,10 @@
     set -e 
 
     SCRIPT_PATH="../scripts"
+    SI_PATHS="../SI"
+    
+    NAME_SPACE="soere"
+    IP_HOST="localhost"    
 
     ################################################################
     # Arbo SI Configuration Ex
@@ -34,8 +38,7 @@
     #
     ### CONFIGURATION ##############################################
     #
-    ################################################################
-    
+    ################################################################    
     
     # SI name
     SI="OLA"
@@ -72,8 +75,7 @@
     ONTOP_MERGE="" # For Merge : "merge" 
     ONTOP_LOG_LEVEL="ALL" # OFF
       
-    CORESE_IGNORE_BREAK_LINE="corese_ignore_line_break" # Empty to disable 
-    
+    CORESE_IGNORE_BREAK_LINE="corese_ignore_line_break" # Empty to disable     
     
     # Corese ARGS
     CORESE_QUERY=${CORESE_QUERY:-"SELECT ?S ?P ?O { ?S ?P ?O . filter( !isBlank(?S) ) . filter( !isBlank(?O) )  } "}
@@ -160,46 +162,150 @@
     # COBY PIPELINE 
     #####################################################
     #####################################################
+     
+    SI=${1:-""}
+   
+    if [ -z "$SI" ] ; then
     
-    SI_PATHS="../SI"
+        for SI in `ls "$SI_PATHS" --ignore "ontology" --ignore "SI.txt" `;   do
+
+         ./synthesis_extractor_process.sh ip=$IP_HOST                                       \
+                                          namespace=$NAME_SPACE                             \
+                                          ro=$RO                                            \
+                                          rw=$RW                                            \
+                                          si=$SI                                            \
+                                          db=$DATA_BASE                                     \
+                                          ext_obda=$EXT_OBDA                                \
+                                          ext_graph=$EXT_GRAPH                              \
+                                          class_file_name=$CLASS_FILE_NAME                  \
+                                          sparql_file_name=$SPARQL_FILE_NAME                \
+                                          csv_file_name=$INPUT_CSV_FILE_NAME                \
+                                          valide_csv_file_name=$OUTPUT_VALIDE_CSV_FILE_NAME \
+                                          csv_sep=$CSV_SEP                                  \
+                                          intra_separators="$INTRA_CSV_SEP"                 \
+                                          columns="$COLUMNSTO_VALIDATE"                     \
+                                          connec_file_name=$CONNEC_FILE_NAME                \
+                                                                                            \
+                                          ontop_xms="$ONTOP_XMS"                            \
+                                          ontop_xmx="$ONTOP_XMX"                            \
+                                          ontop_ttl_format="$ONTOP_TTL_FORMAT"              \
+                                          ontop_batch="$ONTOP_BATCH"                        \
+                                          ontop_page_size="$ONTOP_PAGE_SIZE"                \
+                                          ontop_flush_count="$ONTOP_FLUSH_COUNT"            \
+                                          ontop_merge="$ONTOP_MERGE"                        \
+                                          ontop_query="$ONTOP_QUERY"                        \
+                                          ontop_fragment="$ONTOP_FRAGMENT"                  \
+                                          ontop_log_level=$ONTOP_LOG_LEVEL                  \
+                                                                                            \
+                                          corese_xms="$CORESE_XMS"                          \
+                                          corese_xmx="$CORESE_XMX"                          \
+                                          corese_query="$CORESE_QUERY"                      \
+                                          corese_peek="$CORESE_PEEK"                        \
+                                          corese_fragment="$CORESE_FRAGMENT"                \
+                                          corese_flush_count="$CORESE_FLUSH_COUNT"          \
+                                          corese_format="$CORESE_FORMAT"                    \
+                                          "$CORESE_IGNORE_BREAK_LINE"
+                                          
+        done 
+        
+        $SCRIPT_PATH/02_build_config.sh  ip=$IP_HOST namespace=$NAME_SPACE rw=$RW ro=$RO 
+         
+        $SCRIPT_PATH/11_nano_start_stop.sh start rw 12 12 28
+   
+        echo
+        echo
+        echo 
+        echo -e "\e[93m ######################################################################## \e[32m "
+        echo -e "\e[93m ######################################################################## \e[32m "
+        echo -e "\e[93m ######## LOADING SYNTHESIS DATA ######################################## \e[32m "
+        echo -e "\e[93m ######################################################################## \e[32m "
+        echo -e "\e[93m ######################################################################## \e[32m "
+        echo 
+        echo       
+
+        for SI in `ls "$SI_PATHS" --ignore "ontology" --ignore "SI.txt" `;  do
+
+           if [ -d "$SI_PATHS/$SI/output/04_synthesis/" ] ; then 
+          
+              $SCRIPT_PATH/09_load_data.sh from_directory="$SI_PATHS/$SI/output/04_synthesis/" \
+                                           content_type="text/turtle"           
+           fi
+
+        done          
+         
+        $SCRIPT_PATH/11_nano_start_stop.sh stop
+                  
+        $SCRIPT_PATH/11_nano_start_stop.sh start ro 12 12 28
+  
+        ## read -rsn1        
+        
+    else 
+           ./synthesis_extractor_process.sh ip="$IP_HOST"                                     \
+                                            namespace="$NAME_SPACE"                           \
+                                            ro=$RO                                            \
+                                            rw=$RW                                            \
+                                            si=$SI                                            \
+                                            db=$DATA_BASE                                     \
+                                            ext_obda=$EXT_OBDA                                \
+                                            ext_graph=$EXT_GRAPH                              \
+                                            class_file_name=$CLASS_FILE_NAME                  \
+                                            sparql_file_name=$SPARQL_FILE_NAME                \
+                                            csv_file_name=$INPUT_CSV_FILE_NAME                \
+                                            valide_csv_file_name=$OUTPUT_VALIDE_CSV_FILE_NAME \
+                                            csv_sep=$CSV_SEP                                  \
+                                            intra_separators="$INTRA_CSV_SEP"                 \
+                                            columns="$COLUMNSTO_VALIDATE"                     \
+                                            connec_file_name=$CONNEC_FILE_NAME                \
+                                                                                              \
+                                            ontop_xms="$ONTOP_XMS"                            \
+                                            ontop_xmx="$ONTOP_XMX"                            \
+                                            ontop_ttl_format="$ONTOP_TTL_FORMAT"              \
+                                            ontop_batch="$ONTOP_BATCH"                        \
+                                            ontop_page_size="$ONTOP_PAGE_SIZE"                \
+                                            ontop_flush_count="$ONTOP_FLUSH_COUNT"            \
+                                            ontop_merge="$ONTOP_MERGE"                        \
+                                            ontop_query="$ONTOP_QUERY"                        \
+                                            ontop_fragment="$ONTOP_FRAGMENT"                  \
+                                            ontop_log_level=$ONTOP_LOG_LEVEL                  \
+                                                                                              \
+                                            corese_xms="$CORESE_XMS"                          \
+                                            corese_xmx="$CORESE_XMX"                          \
+                                            corese_query="$CORESE_QUERY"                      \
+                                            corese_peek="$CORESE_PEEK"                        \
+                                            corese_fragment="$CORESE_FRAGMENT"                \
+                                            corese_flush_count="$CORESE_FLUSH_COUNT"          \
+                                            corese_format="$CORESE_FORMAT"                    \
+                                            "$CORESE_IGNORE_BREAK_LINE"
+                                        
+       
+           $SCRIPT_PATH/02_build_config.sh  ip=$IP_HOST namespace=$NAME_SPACE rw=$RW ro=$RO 
+         
+           $SCRIPT_PATH/11_nano_start_stop.sh start rw 12 12 28
+           
+           echo
+           echo
+           echo 
+           echo -e "\e[93m ######################################################################## \e[32m "
+           echo -e "\e[93m ######################################################################## \e[32m "
+           echo -e "\e[93m ######## LOADING SYNTHESIS DATA ######################################## \e[32m "
+           echo -e "\e[93m ######################################################################## \e[32m "
+           echo -e "\e[93m ######################################################################## \e[32m "
+           echo 
+           echo
+         
+           if [ -d "$SI_PATHS/$SI/output/04_synthesis/" ] ; then 
+          
+             $SCRIPT_PATH/09_load_data.sh  from_directory="$SI_PATHS/$SI/output/04_synthesis/" \
+                                           content_type="text/turtle"           
+           fi
+         
+           echo
+           
+           $SCRIPT_PATH/11_nano_start_stop.sh stop
+                  
+           $SCRIPT_PATH/11_nano_start_stop.sh start ro 12 12 28
+  
+           ## read -rsn1
+        
+    fi
     
-    for SI in `ls "$SI_PATHS" --ignore "ontology" --ignore "SI.txt" `;   do
-
-     ./synthesis_extractor_process.sh ip=localhost                                      \
-                                      namespace=soere                                   \
-                                      ro=$RO                                            \
-                                      rw=$RW                                            \
-                                      si=$SI                                            \
-                                      db=$DATA_BASE                                     \
-                                      ext_obda=$EXT_OBDA                                \
-                                      ext_graph=$EXT_GRAPH                              \
-                                      class_file_name=$CLASS_FILE_NAME                  \
-                                      sparql_file_name=$SPARQL_FILE_NAME                \
-                                      csv_file_name=$INPUT_CSV_FILE_NAME                \
-                                      valide_csv_file_name=$OUTPUT_VALIDE_CSV_FILE_NAME \
-                                      csv_sep=$CSV_SEP                                  \
-                                      intra_separators="$INTRA_CSV_SEP"                 \
-                                      columns="$COLUMNSTO_VALIDATE"                     \
-                                      connec_file_name=$CONNEC_FILE_NAME                \
-                                                                                        \
-                                      ontop_xms="$ONTOP_XMS"                            \
-                                      ontop_xmx="$ONTOP_XMX"                            \
-                                      ontop_ttl_format="$ONTOP_TTL_FORMAT"              \
-                                      ontop_batch="$ONTOP_BATCH"                        \
-                                      ontop_page_size="$ONTOP_PAGE_SIZE"                \
-                                      ontop_flush_count="$ONTOP_FLUSH_COUNT"            \
-                                      ontop_merge="$ONTOP_MERGE"                        \
-                                      ontop_query="$ONTOP_QUERY"                        \
-                                      ontop_fragment="$ONTOP_FRAGMENT"                  \
-                                      ontop_log_level=$ONTOP_LOG_LEVEL                  \
-                                                                                        \
-                                      corese_xms="$CORESE_XMS"                          \
-                                      corese_xmx="$CORESE_XMX"                          \
-                                      corese_query="$CORESE_QUERY"                      \
-                                      corese_peek="$CORESE_PEEK"                        \
-                                      corese_fragment="$CORESE_FRAGMENT"                \
-                                      corese_flush_count="$CORESE_FLUSH_COUNT"          \
-                                      corese_format="$CORESE_FORMAT"                    \
-                                      "$CORESE_IGNORE_BREAK_LINE"
-    done 
-
